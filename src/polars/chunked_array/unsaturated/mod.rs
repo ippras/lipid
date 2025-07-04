4035f808f2682f4abf60dd9b848a5dc5ca370565
kgv <kgv@users.noreply.github.com>
2025-07-04 10:56:56 +0300

    9

--- src/polars/chunked_array/unsaturated/mod.rs
+++ src/polars/chunked_array/unsaturated/mod.rs
@@ -8,21 +8,98 @@ use std::{
     sync::LazyLock,
 };
 
-/// Fatty acid chunked array.
-#[derive(Clone, Debug)]
-pub struct FattyAcidListChunked {
-    pub carbons: UInt8Chunked,
-    pub unsaturated: UnsaturatedListChunked,
+pub const INDEX: &str = "Index";
+pub const PARITY: &str = "Parity";
+
+/// Extension methods for [`Series`]
+pub trait SeriesExt {
+    // fn fatty_acids(&self) -> &FattyAcidListChunked {
+    //     self.try_fatty_acid_list().unwrap()
+    // }
+
+    fn fatty_acid(&self) -> FattyAcidChunked {
+        self.try_fatty_acid().unwrap()
+    }
+
+    fn try_fatty_acid(&self) -> PolarsResult<FattyAcidChunked>;
 }
 
-/// Unsaturated chunked array.
-#[derive(Clone, Debug)]
-pub struct UnsaturatedListChunked(ListChunked);
+/// Fatty acid chunked array.
+#[derive(Clone)]
+pub struct FattyAcidChunked {
+    pub formula: Formula,
+    pub double_bounds: ListChunked,
+    pub triple_bounds: ListChunked,
+}
 
-/// Unsaturated chunked array.
+impl FattyAcidChunked {
+    pub fn formula(&self) -> PolarsResult<Vec<((Option<u8>, Option<u8>), Option<u8>)>> {
+        self.formula.atoms()
+    }
+
+    pub fn double_bounds(
+        &self,
+    ) -> impl Iterator<Item = PolarsResult<Option<DoubleBoundChunked>>> + ExactSizeIterator {
+        self.double_bounds.amortized_iter().map(|series| {
+            let Some(series) = series else {
+                return Ok(None);
+            };
+            let r#struct = series.as_ref().struct_()?;
+            let index = r#struct.field_by_name(INDEX)?.i8()?.clone();
+            let parity = r#struct.field_by_name(PARITY)?.bool()?.clone();
+            Ok(Some(DoubleBoundChunked { index, parity }))
+        })
+    }
+
+    pub fn triple_bounds(
+        &self,
+    ) -> impl Iterator<Item = PolarsResult<Option<Int8Chunked>>> + ExactSizeIterator {
+        self.double_bounds.amortized_iter().map(|series| {
+            let Some(series) = series else {
+                return Ok(None);
+            };
+            let index = series.as_ref().i8()?.clone();
+            Ok(Some(index))
+        })
+    }
+}
+
+/// Fatty acid chunked array.
+#[derive(Clone)]
+pub struct Formula(pub StructChunked);
+
+impl Formula {
+    pub fn atoms(&self) -> PolarsResult<Vec<((Option<u8>, Option<u8>), Option<u8>)>> {
+        let carbon = self.0.field_by_name("C")?;
+        let hydrogen = self.0.field_by_name("H")?;
+        let oxygen = self.0.field_by_name("O")?;
+        Ok(carbon
+            .u8()?
+            .iter()
+            .zip(hydrogen.u8()?.iter())
+            .zip(oxygen.u8()?.iter())
+            .collect())
+    }
+}
+
+#[cfg(feature = "atomic")]
+impl Atomic for &Formula {
+    type Output = PolarsResult<UInt8Chunked>;
+
+    #[inline]
+    fn carbons(self) -> PolarsResult<UInt8Chunked> {
+        Ok(self.0.field_by_name("C")?.u8()?.clone())
+    }
+
+    #[inline]
+    fn hydrogens(self) -> PolarsResult<UInt8Chunked> {
+        Ok(self.0.field_by_name("H")?.u8()?.clone())
+    }
+}
+
+/// Double bound chunked array.
 #[derive(Clone, Debug)]
-pub struct UnsaturatedStructChunked {
+pub struct DoubleBoundChunked {
     pub index: Int8Chunked,
     pub parity: BooleanChunked,
-    pub unsaturation: UInt8Chunked,
 }
--- tests/test1.rs
+++ tests/test1.rs
@@ -10,11 +10,11 @@ use std::{collections::BTreeMap, default, fmt::Sign, fs::File};
 fn test() -> Result<()> {
     let mut data_frame = df! {
         "FattyAcid" => df! {
-            "Carbons" => &[
-                16u8,
-                18u8,
-                18u8,
-            ],
+            "Formula" => df! {
+                "C" => UInt8Chunked::new(PlSmallStr::EMPTY, [16u8, 18, 18]),
+                "H" => UInt8Chunked::new(PlSmallStr::EMPTY, [32, 30, 32]),
+                "O" => UInt8Chunked::full(PlSmallStr::EMPTY, 2, 3),
+            }?.into_struct(PlSmallStr::EMPTY),
             "DoubleBonds" => &[
                 Series::new_empty(PlSmallStr::EMPTY, &DataType::Struct(vec![
                     Field::new(PlSmallStr::from_static("Index"), DataType::Int8),
@@ -35,15 +35,14 @@ fn test() -> Result<()> {
                 Series::new_empty(PlSmallStr::EMPTY, &DataType::Int8),
             ],
         }?.into_struct(PlSmallStr::EMPTY),
-        // "Value" => [
-        //     Some(1f64),
-        //     Some(2f64),
-        //     Some(3f64),
-        //     Some(4f64),
-        //     Some(5f64),
-        // ],
-
     }?;
+    // "Value" => [
+    //     Some(1f64),
+    //     Some(2f64),
+    //     Some(3f64),
+    //     Some(4f64),
+    //     Some(5f64),
+    // ],
     println!("data_frame: {data_frame}");
     let mut file = File::create("file.parquet").expect("could not create file");
     let mut writer = ParquetWriter::new(&mut file);
@@ -58,3 +57,28 @@ fn test() -> Result<()> {
     writer.finish(&mut data_frame).unwrap();
     Ok(())
 }
+
+// #[test]
+// fn test1() -> Result<()> {
+//     Series::from_iter([
+//         Series::new_empty(
+//             PlSmallStr::EMPTY,
+//             &DataType::Struct(vec![
+//                 Field::new(PlSmallStr::from_static("Index"), DataType::Int8),
+//                 Field::new(PlSmallStr::from_static("Parity"), DataType::Boolean),
+//             ]),
+//         ),
+//         df! {
+//             "Index" => Series::from_iter([9i8, 12, 15]),
+//             "Parity" => Series::from_iter([false, false, false]),
+//         }?
+//         .into_struct(PlSmallStr::EMPTY)
+//         .into_series(),
+//         df! {
+//             "Index" => Series::from_iter([9i8, 12]),
+//             "Parity" => Series::from_iter([false, false]),
+//         }?
+//         .into_struct(PlSmallStr::EMPTY)
+//         .into_series(),
+//     ]);
+// }
