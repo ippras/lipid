a0f5c2f7b0951aa69f0881e92fbc5e9a68b07b49
kgv <kgv@users.noreply.github.com>
2026-04-28 15:43:39 +0300

    Try new display

--- src/expr/triacylglycerol/display.rs
+++ src/expr/triacylglycerol/display.rs
@@ -14,4 +14,33 @@ impl TriacylglycerolExpr {
             |_, field| Ok(Field::new(field.name().clone(), DataType::String)),
         )
     }
+
+    // pub fn new_display(
+    //     self,
+    //     stereospecificity: Stereospecificity,
+    //     alternate: bool,
+    // ) -> PolarsResult<Expr> {
+    //     let sn1 = self.stereospecific_number1();
+    //     let sn2 = self.stereospecific_number1();
+    //     let sn3 = self.stereospecific_number3();
+    //     match stereospecificity {
+    //         Stereospecificity::Mono if alternate => format_str(
+    //             "{{1:{} & 2:{} & 3:{} | 1:{} & 2:{} & 3:{} | 1:{} & 2:{} & 3:{} | 1:{} & 2:{} & 3:{} | 1:{} & 2:{} & 3:{} | 1:{} & 2:{} & 3:{}}}",
+    //             &[
+    //                 sn1, sn2, sn3, sn1, sn3, sn2, sn2, sn1, sn3, sn2, sn3, sn1, sn3, sn1, sn2, sn3,
+    //                 sn2, sn1,
+    //             ],
+    //         ),
+    //         Stereospecificity::Positional if alternate => format_str(
+    //             "{{1:{} & 2:{} & 3:{} | 1:{} & 2:{} & 3:{}}}",
+    //             &[sn1, sn2, sn3, sn3, sn2, sn1],
+    //         ),
+    //         Stereospecificity::Stereo if alternate => {
+    //             format_str("{{1:{} & 2:{} & 3:{}}}", &[sn1, sn2, sn3])
+    //         }
+    //         Stereospecificity::Mono => format_str("[{};{};{}]", &[sn1, sn2, sn3]),
+    //         Stereospecificity::Positional => format_str("[{}/2;{};{}/2]", &[sn1, sn2, sn3]),
+    //         Stereospecificity::Stereo => format_str("[{}/3;{}/3;{}/3]", &[sn1, sn2, sn3]),
+    //     }
+    // }
 }
